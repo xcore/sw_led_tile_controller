1a7e8dc0a54a02a9b0da801448b8c1f786bc1159
Marcus Nowotny <marcus@interactive-matter.eu>
2012-04-08 12:39:35 +0200

    uh forgot to commit the necessary changes to make it real C

--- app_ledtile/src/ethernet/application_handler/applicationPackageDispatcher.c
+++ app_ledtile/src/ethernet/application_handler/applicationPackageDispatcher.c
@@ -7,7 +7,7 @@
 
 #include "ethApplicationServer.h"
 
-int isValidPacket(s_packetMac& incoming_packet, const unsigned char own_mac_address[], const unsigned char own_ip_address[]) {
+int isValidPacket(s_packetMac* incoming_packet, const unsigned char own_mac_address[], const unsigned char own_ip_address[]) {
 	//currently we are only dealing with UDP packages (TFTP or data update UUDP packages marked by 'XMOS' in the header
 /*
 	 if (getShort(incoming_packet.ethertype) == ETHERTYPE_IP && getChar(i->proto) == PROTO_UDP) {
@@ -18,6 +18,6 @@ int isValidPacket(s_packetMac& incoming_packet, const unsigned char own_mac_addr
 */
 }
 
-int handlePacket(s_packetMac& mac_packet, s_packetMac& outgoing_packet, const unsigned char own_mac_address[], const unsigned char own_ip_address[]) {
+int handlePacket(s_packetMac* mac_packet, s_packetMac* outgoing_packet, const unsigned char own_mac_address[], const unsigned char own_ip_address[]) {
 	return 0;
 }
