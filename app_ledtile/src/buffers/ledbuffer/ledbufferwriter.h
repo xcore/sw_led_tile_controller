c386a7660a41514e3d33a91af5546ec4435bd751
Marcus Nowotny <marcus@interactive-matter.eu>
2012-04-08 14:30:17 +0200

    documentation is always good

--- app_ledtile/src/buffers/ledbuffer/ledbufferwriter.h
+++ app_ledtile/src/buffers/ledbuffer/ledbufferwriter.h
@@ -14,7 +14,22 @@
 #ifndef LEDBUFFERCLIENT_H_
 #define LEDBUFFERCLIENT_H_
 
+/**
+ * Routine to send LED data to the internal led buffer.
+ * The LED data is send as bulk, so the correct organization in rows & cols
+ * must have done before
+ * c - the chanend to the led buffer
+ * pixptr - the position in the led buffer (normally pointing to the begin of a column)
+ * len - the lenght of the data transmitted
+ * buf the data itself, must be at least the lenght of len
+ */
 void sendLedData(chanend c, unsigned pixptr, unsigned len, unsigned char buf[]);
+
+/**
+ * send a latch signal to the buffer.
+ * The latch signal indicates that a frame is constructed by the led data and can be send to the output modules
+ * c - the channel to the led buffer
+ */
 void sendLedLatch(chanend c);
 
 #endif /*LEDBUFFERCLIENT_H_*/
